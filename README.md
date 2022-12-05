c493df0a9b55bbfdaa977ecae74a02656e0cff28
tuncoin <118679994+TunCoin-Team@users.noreply.github.com>
2022-12-05 21:38:43 +0100

    extended TunCoin contract (#1)

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align="center">
+
+  <img src="assets/tuncoin.png" alt="logo" width="200" height="auto" />
+  <h1>Welcome to TunCoin!</h1>
+
+<h4>
+    <a href="https://tuncoin.io">ICO</a>
+  <span> - </span>
+    <a href="https://tuncoin.io/wp/livre-blanc-tuncoin-v1.pdf">WP (FR)</a>
+  <span> - </span>
+    <a href="https://tuncoin.io/wp/white_paper_tuncoin_v1.pdf">WP (EN)</a>
+</div>
+
+<br />
+
--- contracts/TunCoin.sol
+++ contracts/TunCoin.sol
@@ -1,58 +1,234 @@
-/**
- *Submitted for verification at Etherscan.io on 2022-10-12
-*/
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract TunCoin
-{
-    mapping (address => uint) public balances;
-    mapping (address => mapping (address=>uint)) public allowance;
+import "./Context.sol";
+import "./Bep20Interface.sol";
+import "./Ownable.sol";
+import "./SafeMath.sol";
 
-    uint private totalSupply =  31415926 * 10 ** 18;
-    string public name = "TUNCOIN";
-    string public symbol = "TUN";
-    uint public decimals = 18;
+contract TunCoin is Context, Bep20Interface, Ownable {
+  using SafeMath for uint256;
 
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed owner, address indexed spender, uint value);
+  mapping (address => uint256) private _balances;
 
-    constructor(){
-        balances[msg.sender]=totalSupply;
-    }
+  mapping (address => mapping (address => uint256)) private _allowances;
 
-    function balanceOf(address user) public view returns (uint)
-    {
-        return balances[user];
+  uint256 private _totalSupply;
+  uint8 private _decimals;
+  string private _symbol;
+  string private _name;
 
-    }
+  constructor() {
+    _name = "TUNCOIN";
+    _symbol = "TUNC";
+    _decimals = 18;
+    _totalSupply = 31415926 * 10 ** 18;
+    _balances[msg.sender] = _totalSupply;
 
-    function transfer(address to, uint value) public returns (bool)
-    {
-        require(balanceOf(msg.sender)>=value, "Solde insuffisant");
-        balances[to]+=value;
-        balances[msg.sender]-=value;
-        emit Transfer(msg.sender, to, value);
+    emit Transfer(address(0), msg.sender, _totalSupply);
+  }
 
-        return true;
-    }
+  /**
+   * @dev Returns the bep token owner.
+   */
+  function getOwner() external override  view returns (address) {
+    return owner();
+  }
 
-    function TransferFrom(address from, address to, uint value) public  returns (bool)
-    {
-        require(balanceOf(from)>=value, "Solde insuffisant");
-        require(allowance[from][msg.sender]>=value, "Delegation insuffisante");
-        balances[to]+=value;
-        balances[from]-=value;
-        emit Transfer(from, to, value);
+  /**
+   * @dev Returns the token decimals.
+   */
+  function decimals() external override view returns (uint8) {
+    return _decimals;
+  }
 
-        return true;
-    }
+  /**
+   * @dev Returns the token symbol.
+   */
+  function symbol() external override view returns (string memory) {
+    return _symbol;
+  }
 
-    function approve(address spender, uint value) public  returns (bool)
-    {
-        allowance[msg.sender][spender]=value;
-        emit Approval(msg.sender, spender, value);
+  /**
+  * @dev Returns the token name.
+  */
+  function name() external override view returns (string memory) {
+    return _name;
+  }
 
-        return true;
-    }
+  /**
+   * @dev See {BEP20-totalSupply}.
+   */
+  function totalSupply() external override view returns (uint256) {
+    return _totalSupply;
+  }
+
+  /**
+   * @dev See {BEP20-balanceOf}.
+   */
+  function balanceOf(address account) external override view returns (uint256) {
+    return _balances[account];
+  }
+
+  /**
+   * @dev See {BEP20-transfer}.
+   *
+   * Requirements:
+   *
+   * - `recipient` cannot be the zero address.
+   * - the caller must have a balance of at least `amount`.
+   */
+  function transfer(address recipient, uint256 amount) external override returns (bool) {
+    _transfer(_msgSender(), recipient, amount);
+    return true;
+  }
+
+  /**
+   * @dev See {BEP20-allowance}.
+   */
+  function allowance(address owner, address spender) external override view returns (uint256) {
+    return _allowances[owner][spender];
+  }
+
+  /**
+   * @dev See {BEP20-approve}.
+   *
+   * Requirements:
+   *
+   * - `spender` cannot be the zero address.
+   */
+  function approve(address spender, uint256 amount) external override returns (bool) {
+    _approve(_msgSender(), spender, amount);
+    return true;
+  }
+
+  /**
+   * @dev See {BEP20-transferFrom}.
+   *
+   * Emits an {Approval} event indicating the updated allowance. This is not
+   * required by the EIP. See the note at the beginning of {BEP20};
+   *
+   * Requirements:
+   * - `sender` and `recipient` cannot be the zero address.
+   * - `sender` must have a balance of at least `amount`.
+   * - the caller must have allowance for `sender`'s tokens of at least
+   * `amount`.
+   */
+  function transferFrom(address sender, address recipient, uint256 amount) external override returns (bool) {
+    _transfer(sender, recipient, amount);
+    _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "BEP20: transfer amount exceeds allowance"));
+    return true;
+  }
+
+  /**
+   * @dev Atomically increases the allowance granted to `spender` by the caller.
+   *
+   * This is an alternative to {approve} that can be used as a mitigation for
+   * problems described in {BEP20-approve}.
+   *
+   * Emits an {Approval} event indicating the updated allowance.
+   *
+   * Requirements:
+   *
+   * - `spender` cannot be the zero address.
+   */
+  function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+    return true;
+  }
+
+  /**
+   * @dev Atomically decreases the allowance granted to `spender` by the caller.
+   *
+   * This is an alternative to {approve} that can be used as a mitigation for
+   * problems described in {BEP20-approve}.
+   *
+   * Emits an {Approval} event indicating the updated allowance.
+   *
+   * Requirements:
+   *
+   * - `spender` cannot be the zero address.
+   * - `spender` must have allowance for the caller of at least
+   * `subtractedValue`.
+   */
+  function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+    _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "BEP20: decreased allowance below zero"));
+    return true;
+  }
+
+
+  /**
+   * @dev Moves tokens `amount` from `sender` to `recipient`.
+   *
+   * This is internal function is equivalent to {transfer}, and can be used to
+   * e.g. implement automatic token fees, slashing mechanisms, etc.
+   *
+   * Emits a {Transfer} event.
+   *
+   * Requirements:
+   *
+   * - `sender` cannot be the zero address.
+   * - `recipient` cannot be the zero address.
+   * - `sender` must have a balance of at least `amount`.
+   */
+  function _transfer(address sender, address recipient, uint256 amount) internal {
+    require(sender != address(0), "BEP20: transfer from the zero address");
+    require(recipient != address(0), "BEP20: transfer to the zero address");
+
+    _balances[sender] = _balances[sender].sub(amount, "BEP20: transfer amount exceeds balance");
+    _balances[recipient] = _balances[recipient].add(amount);
+    emit Transfer(sender, recipient, amount);
+  }
+
+
+  /**
+   * @dev Destroys `amount` tokens from `account`, reducing the
+   * total supply.
+   *
+   * Emits a {Transfer} event with `to` set to the zero address.
+   *
+   * Requirements
+   *
+   * - `account` cannot be the zero address.
+   * - `account` must have at least `amount` tokens.
+   */
+  function _burn(address account, uint256 amount) internal {
+    require(account != address(0), "BEP20: burn from the zero address");
+
+    _balances[account] = _balances[account].sub(amount, "BEP20: burn amount exceeds balance");
+    _totalSupply = _totalSupply.sub(amount);
+    emit Transfer(account, address(0), amount);
+  }
+
+  /**
+   * @dev Sets `amount` as the allowance of `spender` over the `owner`s tokens.
+   *
+   * This is internal function is equivalent to `approve`, and can be used to
+   * e.g. set automatic allowances for certain subsystems, etc.
+   *
+   * Emits an {Approval} event.
+   *
+   * Requirements:
+   *
+   * - `owner` cannot be the zero address.
+   * - `spender` cannot be the zero address.
+   */
+  function _approve(address owner, address spender, uint256 amount) internal {
+    require(owner != address(0), "BEP20: approve from the zero address");
+    require(spender != address(0), "BEP20: approve to the zero address");
+
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+
+  /**
+   * @dev Destroys `amount` tokens from `account`.`amount` is then deducted
+   * from the caller's allowance.
+   *
+   * See {_burn} and {_approve}.
+   */
+  function _burnFrom(address account, uint256 amount) internal {
+    _burn(account, amount);
+    _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "BEP20: burn amount exceeds allowance"));
+  }
 }
